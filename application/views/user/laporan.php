509508ac6c5559b31291fe0b4aa94837b80f844d
Riomutiara <61374433+Riomutiara@users.noreply.github.com>
2021-09-03 15:36:17 +0700

    0309211536

--- application/views/user/laporan.php
+++ application/views/user/laporan.php
@@ -10,7 +10,7 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="<?= base_url(); ?>assets/img/favicon.png" rel="icon">
+    <link href="<?= base_url(); ?>assets/img/favicon2.png" rel="icon">
     <link href="<?= base_url(); ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
